4945a89584d342aef2b1f9a3bf14f52bf4f421cf
InuTheGreat <konrad.mrozowskii@gmail.com>
2026-08-10 16:04:56 +0200

    test czy podawanie wspołrzędnych działa prawidłowo i jest odporne na błędy

--- kolkoikrzyzyk.py
+++ kolkoikrzyzyk.py
@@ -10,7 +10,7 @@ def print_board(x_board):
         print()
 
 #ustawianie kółka bądź krzyżyka
-def set_symbol(x_board, znak):
+def set_symbol(znak):#będzie jeszcze argument x_board
     if znak == True:
         symbol = 'O'
     else:
@@ -19,17 +19,26 @@ def set_symbol(x_board, znak):
     while ok_or_not==False:
         print( "Gdzie chcesz postawić " + symbol+ "? :" )
         place=input()
-        if len(place)==2 and 0<=int(place[0])<=2 and (place[1].upper in ['A','B','C']):
-            #change board
-            ok_or_not=True
+        if len(place)==2:
+            if 0<=int(place[1])<=2:
+                if place[0].upper() in ['A','B','C']:
+                    #change board
+                    ok_or_not=True
+                else:
+                    print("Niepoprawna wartość. Pierwsza współrzędna powinna być literą A lub B lub C")
+            else:
+                print("Niepoprawna wartość. Druga współrzędna powinna się składać z liczby od 0 do 2.")
         else:
-            print("Niepoprawna wartość.")
+            print("Niepoprawna wartość. Współrzędne powinny składać się z dwóch znaków")
+
+    print("test udany")
 
 
     pass 
 
 #inicjalizacja planszy
 board = [['_' for _ in range(3)]for _ in range(3)]
-
+sign = True
+set_symbol(sign)
 
 print_board(board)
