549b456cb96454f4afe71f419fe82c1c12224355
InuTheGreat <konrad.mrozowskii@gmail.com>
2026-08-10 14:11:11 +0200

    dodanie oznaczen planszy

--- kolkoikrzyzyk.py
+++ kolkoikrzyzyk.py
@@ -1,6 +1,8 @@
 #kółko i krzyżyk
 def print_board(x_board):
+    print("   A B C ")
     for x in range(len(x_board)):
+        print(x,end='  ') 
         for y in range(len(x_board[x])):
             print(x_board[x][y],end=' ')
         print()
