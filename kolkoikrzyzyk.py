2935a2d61c4ab21855661face2d1f810cc1082f6
InuTheGreat <konrad.mrozowskii@gmail.com>
2026-08-13 17:25:19 +0200

    dodatnie sprawdzania czy na planszy juz znajduje sie w danym miejscu znak niestety plansza niepoprawnie sie aktualizuje

--- kolkoikrzyzyk.py
+++ kolkoikrzyzyk.py
@@ -20,11 +20,12 @@ def check_symbol(x_board, znak):
         print( "Gdzie chcesz postawić " + symbol+ "?" )
         print("Pamiętaj aby podać porawnie współrzędne: ")
         place=input()
+        place=place.upper()
         if len(place)==2:
             if 0<=int(place[1])<=2:
-                if place[0].upper() in ['A','B','C']:
-                    #change board
-                    ok_or_not=True
+                if place[0] in ['A','B','C']:
+                    t_place = translate_coord(place)
+                    ok_or_not = change_board(x_board, t_place, symbol)
                 else:
                     print("Niepoprawna wartość. Pierwsza współrzędna powinna być literą A lub B lub C")
             else:
@@ -32,24 +33,32 @@ def check_symbol(x_board, znak):
         else:
             print("Niepoprawna wartość. Współrzędne powinny składać się z dwóch znaków")
 
-    print("test udany")
+    
 
 
-def change_board(x_board, symbol):
-    pass
+def change_board(x_board,miejsce,symbol):
+    loop_check = False
+    if x_board[miejsce[0]][miejsce[1]]=='_':
+        x_board[miejsce[0]][miejsce[1]]=symbol
+        loop_check = True
+    else:
+        print("Na tym miejscu stoi już inna figura!")
+    return loop_check
 
-def translate_coord(coord):
+def translate_coord(coord): #zamieniamy współrzędne typu litery na cyfry
     coord_dict = {
         'A':0,
         'B':1,
         'C':2
     }
+    translated_coord = [coord_dict[coord[0]],int(coord[1])]
+    return translated_coord
 
-    
-    pass
+
+while True:
 #inicjalizacja planszy
-board = [['_' for _ in range(3)]for _ in range(3)]
-sign = True
-check_symbol(sign)
+    board = [['_' for _ in range(3)]for _ in range(3)]
+    sign = True
+    check_symbol(board, sign)
 
-print_board(board)
+    print_board(board)
