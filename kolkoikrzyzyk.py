a778fb1373c4a3f506cdfadefeb7b411913cc6a3
InuTheGreat <konrad.mrozowskii@gmail.com>
2026-08-13 17:42:49 +0200

    dodanie stawiania znaków na planszy nadal znak sie nie zmienia z O na X

--- kolkoikrzyzyk.py
+++ kolkoikrzyzyk.py
@@ -6,7 +6,7 @@ def print_board(x_board):
     for x in range(len(x_board)):
         print(x,end='  ') 
         for y in range(len(x_board[x])):
-            print(x_board[x][y],end=' ')
+            print(x_board[y][x],end=' ')
         print()
 
 #wpisujemy O bądź X i sprawdzamy, czy współrzędne są poprawne
@@ -25,7 +25,14 @@ def check_symbol(x_board, znak):
             if 0<=int(place[1])<=2:
                 if place[0] in ['A','B','C']:
                     t_place = translate_coord(place)
-                    ok_or_not = change_board(x_board, t_place, symbol)
+                    if check_if_present(x_board, t_place):
+                        x_board=change_board(x_board,symbol,t_place)
+                        if symbol == True:
+                            symbol = False
+                        else:
+                            symbol=True
+                        ok_or_not=True
+                        return x_board
                 else:
                     print("Niepoprawna wartość. Pierwsza współrzędna powinna być literą A lub B lub C")
             else:
@@ -36,10 +43,9 @@ def check_symbol(x_board, znak):
     
 
 
-def change_board(x_board,miejsce,symbol):
+def check_if_present(x_board,miejsce):
     loop_check = False
     if x_board[miejsce[0]][miejsce[1]]=='_':
-        x_board[miejsce[0]][miejsce[1]]=symbol
         loop_check = True
     else:
         print("Na tym miejscu stoi już inna figura!")
@@ -55,10 +61,15 @@ def translate_coord(coord): #zamieniamy współrzędne typu litery na cyfry
     return translated_coord
 
 
-while True:
+def change_board(x_board,symbol,place):
+    x_board[place[0]][place[1]]=symbol
+    return x_board
+
 #inicjalizacja planszy
-    board = [['_' for _ in range(3)]for _ in range(3)]
+board = [['_' for _ in range(3)]for _ in range(3)]
+while True:#główna pętla gry
+
     sign = True
-    check_symbol(board, sign)
+    board = check_symbol(board, sign)
 
     print_board(board)
